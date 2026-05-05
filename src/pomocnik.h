0eb4b86f59805341f6330013c904467c60b56509
bijakpawel <pawel0bijak@gmail.com>
2026-05-05 20:31:02 +0200

    Pomocnik: dodaj funkcje wypelnijKolejke do losowego wypelniania

--- src/pomocnik.h
+++ src/pomocnik.h
@@ -43,3 +43,18 @@ double zmierzCzas(Funkcja operacja) {
     operacja();
     return static_cast<double>(zegar.stop());
 }
+
+// Wypelnia kolejke priorytetowa n losowymi elementami.
+// Wartosci losowane sa z zakresu [-1000, 1000], priorytety z zakresu [0, zakresPriorytetow-1].
+// Zaleca sie aby zakresPriorytetow byl kilkukrotnie wiekszy od n (zgodnie z trescia projektu).
+template <typename Kolejka>
+void wypelnijKolejke(Kolejka& kolejka, int n, int zakresPriorytetow) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distWart(-1000, 1000);
+    std::uniform_int_distribution<> distPrio(0, zakresPriorytetow - 1);
+
+    for (int i = 0; i < n; i++) {
+        kolejka.insert(distWart(gen), distPrio(gen));
+    }
+}
